73c1b3155b811719980c4d2957ac8a1c16aec26b
Jackson Guerrero <jacksonguerrerovega2@gmail.com>
2021-07-27 17:59:12 -0500

    Diseño modal

--- src/components/ModalMovie.jsx
+++ src/components/ModalMovie.jsx
@@ -1,13 +1,30 @@
 import React from 'react'
-import { ContentModal, Modal } from './modal-movie-styled/modalMovieStyled'
+import { ContentCard, Etiqueta, ImgFilm } from './card-film-styled/CardFilmStyled'
+import CardFilm from './CardFilm'
+import { ButtonBack, ContentButtonAction, ContentDetalle, ContentModal, ImgFilmModal, Modal } from './modal-movie-styled/modalMovieStyled'
 
 const ModalMovie = ({dataModal, setShowModal}) => {
+    console.log(dataModal)
     return (
         <Modal>
             <ContentModal>
-                <button onClick={()=> {setShowModal(false)}}>xd</button>
-                <h2>Mi primer Modal</h2>
-                <p>{dataModal.title}</p>
+                <ButtonBack onClick={()=> {setShowModal(false)}}>X</ButtonBack>
+                <ContentCard>
+                    {/* <Etiqueta raiting={dataModal.vote_average}>
+                        <img src="https://i.imgur.com/sitPeIs.png" alt="Puntuación" />
+                        {' '+dataModal.vote_average}
+                    </Etiqueta> */}
+                    <ImgFilmModal src={`https://image.tmdb.org/t/p/w500${dataModal.poster_path}`} alt='Imagen'/>
+                </ContentCard>
+                <ContentDetalle>
+                    <h2>{dataModal.original_title}</h2>
+                    <p>{dataModal.overview}</p>
+                    <span>{dataModal.release_date}</span>
+                    <ContentButtonAction>
+                        <button>Ver ahora</button>
+                        <button>Ver más tarde</button>
+                    </ContentButtonAction>
+                </ContentDetalle>
             </ContentModal>  
         </Modal>
     )
--- src/components/modal-movie-styled/modalMovieStyled.js
+++ src/components/modal-movie-styled/modalMovieStyled.js
@@ -3,19 +3,63 @@ import styled from 'styled-components'
 
 export const ContentModal = styled.div`
     background-color: transparent;
-    width:300px;
+    width: 90%;
     padding: 10px 20px;
-    margin: 20% auto;
+    margin: 10% auto;
     position: relative;
+    display: grid;
+    grid-template-columns: 1fr 2fr;
+    text-align: center;
 `
 export const Modal = styled.div`
     background-color: rgba(0,0,0,.8);
     position:fixed;
     top:0;
     right:0;
-    bottom:0;
     left:0;
+    bottom: 0;
     opacity:1;
     transition: all 1s;
     z-index: 100;
+`
+export const ButtonBack = styled.button`
+    background-color: antiquewhite;
+    grid-column: 1/4;
+    width: 10%;
+    height: 2rem;
+    position: absolute;
+    right: 0;
+    border: 0;
+    background-color: #FED941;
+    border-radius: 10px;
+`
+export const ImgFilmModal = styled.img`
+    margin: auto;
+    width: 180px;
+    height: 250px;
+    object-fit: cover;
+    border-radius: 1rem;
+
+`
+
+export const ContentDetalle = styled.div`
+    padding: 2rem;
+    h2{
+        color: white;
+    }
+    p{
+        color: white;
+    }
+`
+
+export const ContentButtonAction = styled.div`
+    display: flex;
+    justify-content: space-around;
+    button{
+        border: 0;
+        background-color: #FED941;
+        border-radius: 10px;
+        width: 40%;
+        padding: 0.4rem 1rem;
+    }
 `
