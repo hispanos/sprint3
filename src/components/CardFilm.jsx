0d723ca497bc122f3559bff86583962294e95895
Jackson Guerrero <jacksonguerrerovega2@gmail.com>
2021-07-27 12:50:57 -0500

    condicional raiting color

--- src/components/CardFilm.jsx
+++ src/components/CardFilm.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
 import { ContentCard, Etiqueta, ImgFilm } from './card-film-styled/CardFilmStyled'
-const raiting = 2
-const colorT = raiting > 50? '#0d0dff' : '#ff8801'
+const raiting = 90
 const CardFilm = () => {
     return (
         <ContentCard>
-            <Etiqueta color={colorT}>"star"{raiting}</Etiqueta>
+            <Etiqueta raiting={raiting}>"star"{raiting}</Etiqueta>
             <ImgFilm src='https://es.web.img2.acsta.net/pictures/19/11/12/12/25/0815514.jpg' alt=''/>
         </ContentCard>
     )
